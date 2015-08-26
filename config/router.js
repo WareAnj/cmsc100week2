00fe4b8926182e171459d9b054f76e7d3519bcb7
ware.angelicap@gmail.com <ware.angelicap@gmail.com>
2015-08-26 12:24:33 +0800

    cmsc100week3 modifications

--- config/router.js
+++ config/router.js
@@ -3,15 +3,21 @@ var teacher = require('./../controllers/teacher');
 
 module.exports = function(router){				//if we want to export functions to other modules
 	router.route('/students')					//route handler
-		.get(student.find);						//
+		.get(student.find)
+		.post(student.insert);					//
+	router.route('/students/:id')
+		.get(student.findOne)
+		.put(student.update);
 
 	return router;
 };
-
+/*
 module.exports = function(router){				//if we want to export functions to other modules
 	router.route('/teachers')					//route handler
 		.get(teacher.find)						
 		.post(teacher.add);
 	return router;
 };
+*/
+
 
