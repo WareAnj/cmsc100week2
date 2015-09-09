4d000c0432ab0ffd1414c356ddc498266dfd1f18
WareAnj <ware.angelicap@gmail.com>
2015-09-09 10:50:37 +0800

    Modified test/student.js

--- test/student.js
+++ test/student.js
@@ -46,13 +46,12 @@ describe('Student', function(){
 
 	describe("insert()", function(){
 		it('should be able to add a student and a student number', function(done){
-			request(url)
 			studrec = 
 			{
-				id,
 				name: 'Angelica',
 				studNo: '2013-10457'
 			}
+			request(url)
 			.post('/students')
 			.send(studrec)
 			.end(function(err, res){
@@ -62,8 +61,8 @@ describe('Student', function(){
 				studrec.res.body.should.have.property('id')
 				studrec.res.body.should.have.property('name')
 				studrec.res.body.should.have.property('studNo').with.lengthOf(10)
-				studrec.res.body.('name').should.be.equal('Angelica')
-				studrec.res.body.('studNo').should.be.equal('2013-10457')
+				//studrec.res.body.('name').should.be.equal('Angelica')
+				//studrec.res.body.('studNo').should.be.equal('2013-10457')
 				done();
 			});
 		});
